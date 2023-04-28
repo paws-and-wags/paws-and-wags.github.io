ce5e8fba96592d87cecf9550124fb1aba940eb22
kiransukumaran <ks4uofficial@gmail.com>
2023-04-28 19:40:13 +0530

    Update launch date

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("May 1, 2023 10:00:00").getTime();
+        var finalDate =  new Date("May 15, 2023 10:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
